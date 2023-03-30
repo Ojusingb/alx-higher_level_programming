fee1c0511c7b776347044e3f36cbfc35c4635b63
ojusingb <ojusingb@gmail.com>
2023-03-29 20:17:13 -0700

    class square created

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""class that defines a square"""
 class Square:
-	pass 
+	"""an empty square"""
+	pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""define class of square"""
 class Square:
+	"""a square Attributes:
+	__size (int): size of a side of the square"""
 	def __init__(self, size):
-
+		"""initializes a square
+		args:
+		size (int): size of a side of the square
+		Returns: none"""
 		self.__size = size
